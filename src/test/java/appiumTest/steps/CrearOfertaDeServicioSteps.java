ab226e94bd77e971c7f2aab969486aceb0d430e4
rakidwam_devs <devs@econtinuum.cl>
2021-03-05 12:47:40 -0300

    no message

---
+++ src/test/java/appiumTest/steps/CrearOfertaDeServicioSteps.java
@@ -0,0 +1,36 @@
+package appiumTest.steps;
+
+import appiumTest.BaseTest;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CrearOfertaDeServicioSteps extends BaseTest {
+	
+	
+	@Given("^Completar login prestador y entrar$")
+	public void completar_login_prestador_y_entrar() throws Throwable {
+	    
+	}
+	@And("^Hacer click en pestana Servicios$")
+	public void hacer_click_en_pestana_Servicios() throws Throwable {
+	   
+	}
+
+	@When("^Hacer click en boton agregar servicio$")
+	public void hacer_click_en_boton_agregar_servicio() throws Throwable {
+	    
+	}
+
+	@And("^Ingresar datos del servicio$")
+	public void ingresar_datos_del_servicio() throws Throwable {
+	    
+	}
+
+	@Then("^Hacer click en boton registrar servicio$")
+	public void hacer_click_en_boton_registrar_servicio() throws Throwable {
+	    
+	}
+
+}
