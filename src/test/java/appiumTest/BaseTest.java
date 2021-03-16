bf2b005198ea3d4e373453b85ec9580b947e4dd3
Nfsoto <nfsoto97@gmail.com>
2021-03-15 23:25:56 -0300

    save functions basetest

--- src/test/java/appiumTest/BaseTest.java
+++ src/test/java/appiumTest/BaseTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.URL;
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -127,6 +128,96 @@ public class BaseTest {
 	            driver.quit();
 	        }
 	    }
+	 
+		
+		public void funcionScrollUp() throws InterruptedException {
+			driver.manage().window().getSize();
+			Dimension dimension = driver.manage().window().getSize();
+			int start_x = (int) (dimension.width * 0.1);
+			int start_y = (int) (dimension.height * 0.4);
+			
+			int end_x = (int) (dimension.width * 0.1);
+			int end_y = (int) (dimension.height * 0.8);
+			
+			TouchAction touch = new TouchAction(driver);
+			touch.press(PointOption.point(start_x, start_y))
+			.waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+			.moveTo(PointOption.point(end_x, end_y)).release().perform();
+			Thread.sleep(3000);
+		}
+		
+		public void funcionScrollDown(Double height_start,Double height_end ) throws InterruptedException {
+			driver.manage().window().getSize();
+			Dimension dimension = driver.manage().window().getSize();
+			int start_x = (int) (dimension.width * 0.5);
+			int start_y = (int) (dimension.height * height_start);
+			
+			int end_x = (int) (dimension.width * 0.5);
+			int end_y = (int) (dimension.height * height_end);
+			
+			TouchAction touch = new TouchAction(driver);
+			touch.press(PointOption.point(start_x, start_y))
+			.waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+			.moveTo(PointOption.point(end_x, end_y)).release().perform();
+			Thread.sleep(3000);
+		}
+		
+		public void funcionScrollHorizontal(Double height_start,Double height_end ) throws InterruptedException {
+			driver.manage().window().getSize();
+			Dimension dimension = driver.manage().window().getSize();
+			int start_x = (int) (dimension.width * height_start);
+			int start_y = (int) (dimension.height * 0.5);
+			
+			int end_x = (int) (dimension.width * height_end);
+			int end_y = (int) (dimension.height * 0.5);
+			
+			TouchAction touch = new TouchAction(driver);
+			touch.press(PointOption.point(start_x, start_y))
+			.waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+			.moveTo(PointOption.point(end_x, end_y)).release().perform();
+			Thread.sleep(3000);
+		}
+		
+		 public MobileElement elementExist(String xpath)
+	     {
+	         try
+	         {
+	             return driver.findElementByXPath(xpath);
+	         }
+	         catch (Exception e)
+	         {
+	             return null;
+	         }
+
+	     }
+		 
+		 public MobileElement exist(By element)
+	     {
+	         try
+	         {
+	             return driver.findElement(element);
+	         }
+	         catch (Exception e)
+	         {
+	             return null;
+	         }
+
+	     }
+		 
+		 public String elementExistSolicitud(Integer n)
+	     {
+	         try
+	         {	By elemento=By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup["+n+"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.TextView[1]");
+	         String catServicio= driver.findElement(elemento).getAttribute("text");
+	             return catServicio;
+	             
+	         }
+	         catch (Exception e)
+	         {
+	             return null;
+	         }
+
+	     }	
 	
 	
 	
