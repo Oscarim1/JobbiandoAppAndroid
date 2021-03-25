927f0fc51d85735c53666aa2e24287821fe50402
rakidwam_devs <devs@econtinuum.cl>
2021-03-25 20:06:15 -0300

    Commit sincronización pruebas samsung.

--- src/test/java/pages/RegistroSolicitantePage.java
+++ src/test/java/pages/RegistroSolicitantePage.java
@@ -13,13 +13,22 @@ import io.appium.java_client.MobileElement;
 public class RegistroSolicitantePage extends BaseTest {
 	MobileElement elemento=null; 
 	By btnQuieroSerJobber = By.xpath("//*[@text=\"crear cuenta aquí\"]");
-	By btnAccept=By.id("com.android.chrome:id/terms_accept");
-	By btnNext=By.id("com.android.chrome:id/next_button");
-	By btnNoThanks=By.id("com.android.chrome:id/negative_button");
+	////////////////////VALIDACION CHROME//////////////////////////
+	By btnAccept = By.id("com.android.chrome:id/terms_accept");	
+	By btnNext = By.id("com.android.chrome:id/next_button");
+	By btnNoThanks = By.id("com.android.chrome:id/negative_button");
+	//////////////////FIN VALIDACION CHROME////////////////////////
+	By btnAcepto = By.xpath("//*[@resource-id=\"com.android.chrome:id/terms_accept\"]");
+	By btnNo = By.xpath("//*[@resource-id=\"com.android.chrome:id/negative_button\"]");
+	By btnSearch = By.xpath("//*[@resource-id=\"com.android.chrome:id/search_box_text\"]");
+	By btnLink = By.xpath("//*[@text=\"app.jobbiando.cl/registroPrestador\"]");
+	By btnCerrar = By.xpath("//*[@resource-id=\"com.android.chrome:id/infobar_close_button\"]");
+	By btnBlock = By.xpath("//*[@text=\"BLOCK\"]");
+	By btnAllow = By.xpath("//*[@text=\"ALLOW\"]");
 	By txtNombres=By.xpath("//*[@resource-id=\"mat-input-0\"]");
 	By txtApellidos=By.xpath("//*[@resource-id=\"mat-input-1\"]");
-	By txtFechaNacimiento = By.xpath("//*[@resource-id=\"mat-input-2\"]");
-	By btnBlock = By.xpath("//*[@text=\"BLOCK\"]");
+	By txtFechaNacimiento=By.xpath("//*[@resource-id=\"fecha_nacimiento\"]");
+	
 	By btnFormulario = By.xpath("//*[@resource-id=\"register-form\"]");
 	By txtCorreo=By.xpath("//*[@resource-id=\"mat-input-3\"]");
 	By txtContrasena=By.xpath("//*[@resource-id=\"password\"]");
@@ -27,33 +36,28 @@ public class RegistroSolicitantePage extends BaseTest {
 	By btnTipoDocumento = By.xpath("//*[@resource-id=\"selectTipo\"]");
 	By btnCedulaDeIdentidadOpcion0 = By.xpath("//*[@resource-id=\"selectTipo\"]");
 	By txtNumeroIdentificador= By.xpath("//*[@resource-id=\"mat-input-6\"]");
-	By txtNumeroSerie= By.xpath("//*[@resource-id=\"mat-input-7\"]");
+	By txtNumeroSerie= By.xpath("//*[@resource-id=\"numero_serie\"]");
 	By txtCalle= By.xpath("//*[@resource-id=\"mat-input-8\"]");
 	By txtDireccion= By.xpath("//*[@resource-id=\"mat-input-9\"]");
 	By txtNombreDireccion= By.xpath("//*[@resource-id=\"mat-input-10\"]");
 	By txtReferencia= By.xpath("//*[@resource-id=\"mat-input-11\"]");
-	By btnRegistrar= By.xpath("//*[@resource-id=\"btnSaveRegistro\"]");
+	By btnRegistrar= By.xpath("//*[@resource-id=\"btnSaveRegistro\"]"); 
+	
 	
 	public RegistroSolicitantePage(MobileElement elemento) 
 	{
 		this.elemento=elemento;
 	}
-	
-	public void clickQuieroSerJobber() throws InterruptedException {
-		Thread.sleep(5000);
-		driver.findElement(btnQuieroSerJobber).click();
-		Thread.sleep(10000);
-	}
-	public void validacionSamsung() throws InterruptedException{
-		
+	////////////////////////VALIDACION SAMSUNG///////////////////////////////////
+	public void validacionSamsung() throws InterruptedException
+	{	
 		if(exist(btnAccept)!=null) {
 			driver.findElement(btnAccept).click();
 			Thread.sleep(5000);
 			driver.findElement(btnNext).click();
 			Thread.sleep(5000);
 			driver.findElement(btnNoThanks).click();
-			Thread.sleep(5000);
-			
+			Thread.sleep(5000);		
 		}
 		else if(exist(btnNext)!=null) {
 			driver.findElement(btnNext).click();
@@ -66,22 +70,59 @@ public class RegistroSolicitantePage extends BaseTest {
 		}
 		
 	}
+	public void iniciarSesion() throws InterruptedException {
+		
+		if(exist(btnAcepto)!=null) {
+			driver.findElement(btnAcepto).click();
+			Thread.sleep(15000);
+			driver.findElement(btnNext).click();
+			Thread.sleep(15000);
+			driver.findElement(btnNo).click();
+			Thread.sleep(15000);
+			driver.findElement(btnSearch).sendKeys("app.jobbiando.cl/registroPrestador");
+			Thread.sleep(15000);
+			driver.findElement(btnLink).click();
+			Thread.sleep(15000);
+			if(exist(btnCerrar)!=null) {
+				driver.findElement(btnCerrar).click();
+				Thread.sleep(15000);
+			}
+			
+			
+		}
+		else {
+			driver.findElement(btnSearch).sendKeys("app.jobbiando.cl/registroPrestador");
+			Thread.sleep(15000);
+			driver.findElement(btnLink).click();
+			Thread.sleep(15000);
+			if(exist(btnCerrar)!=null) {
+				driver.findElement(btnCerrar).click();
+				Thread.sleep(3000);
+			}
+			
+		}
+				
+	}
 	
-	///////////////////////////////////////////////////////////
-	public void enterNombres(String nombres) throws InterruptedException 
+	public void enterNombres(String nombres) throws InterruptedException
 	{	
 		if(exist(btnBlock)==null) {
 			driver.findElement(txtNombres).sendKeys(nombres);
 			Thread.sleep(5000);
+			
 		}else {
 			driver.findElement(btnBlock).click();
-		    Thread.sleep(5000);
+		    Thread.sleep(3000);
+		    driver.findElement(btnCerrar).click();
+			Thread.sleep(3000);
+		    System.out.println(nombres);
 			driver.findElement(txtNombres).sendKeys(nombres);
-			Thread.sleep(5000);
+			Thread.sleep(3000);
 		}
+		
 	}
 	
-	public void enterApellidos(String apellidos) throws InterruptedException 
+	public void enterApellidos(String apellidos) throws InterruptedException
 	{
 		driver.findElement(txtApellidos).sendKeys(apellidos);
 		Thread.sleep(5000);
@@ -93,13 +134,12 @@ public class RegistroSolicitantePage extends BaseTest {
 		Thread.sleep(3000);
 		driver.findElement(txtFechaNacimiento).click();
 		Thread.sleep(3000);
-		this.funcionScrollUp();
-		Thread.sleep(5000);
+		this.funcionScrollUp();	
 		driver.findElement(btnFormulario).click();
 		Thread.sleep(3000);
 	}
 	
-	public void enterCorreo(String correo) throws InterruptedException 
+	public void enterCorreo(String correo) throws InterruptedException
 	{
 		driver.findElement(txtCorreo).sendKeys(correo);
 		Thread.sleep(5000);
@@ -109,14 +149,13 @@ public class RegistroSolicitantePage extends BaseTest {
 	{
 		
 		this.funcionScrollDown(0.8,0.6);
-	    Thread.sleep(5000);
 		driver.findElement(txtContrasena).sendKeys(contrasena);
 		Thread.sleep(5000);
 		driver.findElement(txtContrasena2).sendKeys(contrasena);
 		Thread.sleep(5000);
 	}
 	
-	public void clickCedulaDeIdentidad(String numeroIdentificador)throws InterruptedException  {
+	public void clickCedulaDeIdentidad(String numeroIdentificador)throws InterruptedException {
 		driver.findElement(btnTipoDocumento).click();
 		Thread.sleep(5000);
 		driver.findElement(btnCedulaDeIdentidadOpcion0).click();
@@ -141,13 +180,16 @@ public class RegistroSolicitantePage extends BaseTest {
 		Thread.sleep(5000);
 	}
 	
-	public void enterDireccion(String direccion) throws InterruptedException 
+	public void enterDireccion(String direccion) throws InterruptedException
 	{
 		driver.findElement(txtDireccion).sendKeys(direccion);
 		Thread.sleep(5000);
+		//driver.findElement(txtDireccion).click();
+		//Thread.sleep(5000);
+
 	}
 	
-	public void enterNombreDireccion(String nombreDireccion) throws InterruptedException 
+	public void enterNombreDireccion(String nombreDireccion) throws InterruptedException
 	{
 		driver.findElement(txtNombreDireccion).sendKeys(nombreDireccion);
 		Thread.sleep(5000);
@@ -155,18 +197,19 @@ public class RegistroSolicitantePage extends BaseTest {
 	
 	public void enterReferencia(String referencia) throws InterruptedException 
 	{
-		this.funcionScrollDown(0.9,0.1);
+		this.funcionScrollDown(0.8,0.6);
 		Thread.sleep(5000);
 		driver.findElement(txtReferencia).sendKeys(referencia);
-		System.out.println(referencia);
 		Thread.sleep(5000);
 	}
 	
 	public void clickCrearCuenta() throws InterruptedException {
+		
+		this.funcionScrollDown(0.9,0.1);
 		driver.findElement(btnRegistrar).click();
 		System.out.println("AQUIII");
 		Thread.sleep(5000);
-		
 	}
 	
 }
+
--- src/test/resources/features/01_registroPrestador.feature
+++ src/test/resources/features/01_registroPrestador.feature
@@ -2,7 +2,7 @@
 Feature: Validar la funcionalidad de registro de Prestador
 
   Scenario: Validar registro de Prestador
-    Given Abrir crhome prestador
+    Given Abrir chrome prestador
     And Ir al registro del Prestador
 		When Completar el formulario de registro del prestador
     Then click en boton registrar prestador
